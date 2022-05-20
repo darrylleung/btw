e847efd80061c7371cee3cebebb2e9b90b46c0db
Darryl Leung <darryl.leung@gmail.com>
2022-05-20 13:42:10 +0200

    update to heroku

--- server/server.js
+++ server/server.js
@@ -8,8 +8,8 @@ const io = require("socket.io")(server, {
         callback(
             null,
             req.headers.referer.startsWith(
-                // "https://b-t-w.herokuapp.com/"
-                "http://localhost:3000"
+                "https://b-t-w.herokuapp.com/"
+                // "http://localhost:3000"
             )
         ),
 });
